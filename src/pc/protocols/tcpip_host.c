587bf7be577fbe4747a6c66cf066f839019ef923
Murtadha Bazli Tukimat <murtadhabazlitukimat@gmail.com>
2021-06-17 03:48:12 +0800

    Add device discovery functionality

---
+++ src/pc/protocols/tcpip_host.c
@@ -0,0 +1,181 @@
+/**
+ * @file    tcpip_host.c
+ * @brief   TCP/IP helper definitions
+*/
+
+/* **************************************************************************/
+/*      Include Files                                                       */
+/* **************************************************************************/
+#include <string.h>
+#include <time.h>
+
+#include "tcpip_host.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+
+/* **************************************************************************/
+/*      Private Function Definitions                                        */
+/* **************************************************************************/
+static tcpipHostDeviceState_t tcpip_convert_device_state(uint32_t state)
+{
+    if(state == X_LINK_BOOTED)
+    {
+        return TCPIP_HOST_STATE_BOOTED;
+    }
+    else if(state == X_LINK_UNBOOTED)
+    {
+        return TCPIP_HOST_STATE_BOOTLOADER;
+    }
+    else
+    {
+        return TCPIP_HOST_STATE_UNSUPPORTED;
+    }
+}
+
+static tcpipHostError_t tcpip_create_socket(char* iface_name, void *fd)
+{
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0)
+    {
+        return TCPIP_HOST_ERROR;
+    }
+
+    // add socket option for broadcast
+    int rc = 1;
+    if(setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &rc, sizeof(rc)) < 0)
+    {
+        return TCPIP_HOST_ERROR;
+    }
+
+    int reuse_addr = 1;
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr, sizeof(int)) < 0)
+    {
+        return TCPIP_HOST_ERROR;
+    }
+
+    struct timeval read_timeout;
+    read_timeout.tv_sec = 0;
+    read_timeout.tv_usec = MSEC_TO_USEC(DEVICE_RES_TIMEOUT_MSEC);
+    if(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof(read_timeout)) < 0)
+    {
+        return TCPIP_HOST_ERROR;
+    }
+
+    memcpy(fd, &sockfd, sizeof(sockfd));
+    return TCPIP_HOST_SUCCESS;
+}
+
+/* **************************************************************************/
+/*      Public Function Definitions                                         */
+/* **************************************************************************/
+tcpipHostError_t tcpip_close_socket(void *fd)
+{
+    intptr_t sockfd = (intptr_t)fd;
+    if(sockfd != -1)
+    {
+        close(sockfd);
+        return TCPIP_HOST_SUCCESS;
+    }
+    return TCPIP_HOST_ERROR;
+}
+
+tcpipHostError_t tcpip_get_ip(tcpipHostDeviceInfo_t *devices, uint8_t* device_count, const char* target_ip)
+{
+    // get all network interface information
+    struct ifaddrs *ifaddr;
+    if(getifaddrs(&ifaddr) < 0)
+    {
+        return TCPIP_HOST_ERROR;
+    }
+
+    // iterate linked list of interface information
+    int family;
+    int socket_count = 0;
+    int socket_lookup[MAX_DEVICE_DISCOVERY_IFACE] = {0};
+    for(struct ifaddrs *ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        // check for ipv4 family
+        family = ifa->ifa_addr->sa_family;
+        if(family == AF_INET)
+        {
+            // create new socket for every interface
+            int sockfd = -1;
+            tcpip_create_socket(ifa->ifa_name, &sockfd);
+            socket_lookup[socket_count] = sockfd;
+            socket_count++;
+
+            // get broadcast address
+            char ip_addr[MAX_IP_ADDR_CHAR] = {0};
+            inet_ntop(family, &((struct sockaddr_in *)ifa->ifa_ifu.ifu_broadaddr)->sin_addr, ip_addr, sizeof(ip_addr));
+
+            // send broadcast message
+            struct sockaddr_in broadcast_addr;
+            broadcast_addr.sin_family = family;
+            broadcast_addr.sin_addr.s_addr = inet_addr(ip_addr);
+            broadcast_addr.sin_port = htons(BROADCAST_UDP_PORT);
+
+            tcpipHostCommand_t send_buffer = TCPIP_HOST_CMD_DEVICE_DISCOVER;
+            if(sendto(sockfd, &send_buffer, sizeof(send_buffer), 0, (struct sockaddr *) &broadcast_addr, sizeof(broadcast_addr)) < 0)
+            {
+                return TCPIP_HOST_ERROR;
+            }
+        }
+    }
+
+    // loop to receive message response from device
+    int index = 0;
+    for(int i = 0; i < socket_count; i++)
+    {
+        char ip_addr[MAX_IP_ADDR_CHAR] = {0};
+        tcpipHostDeviceDiscoveryResp_t recv_buffer;
+        struct sockaddr_in dev_addr;
+        uint32_t len = sizeof(dev_addr);
+
+        time_t start_time = time(NULL);
+        do {
+            if(recvfrom(socket_lookup[i], &recv_buffer, sizeof(recv_buffer), 0, (struct sockaddr *) &dev_addr, &len) > 0)
+            {
+                if(recv_buffer.state != X_LINK_UNBOOTED)
+                {
+                    // convert IP address in binary into string
+                    inet_ntop(AF_INET, &dev_addr.sin_addr, ip_addr, sizeof(ip_addr));
+
+                    // copy IP address into buffer
+                    memcpy(((tcpipHostDeviceInfo_t*)(devices)+index)->desc.name, ip_addr, sizeof(ip_addr));
+                    memcpy(((tcpipHostDeviceInfo_t*)(devices)+index)->info.mxid, recv_buffer.mxid, MAX_MXID_CHAR);
+
+                    // convert device status into proper tcpip status
+                    tcpipHostDeviceState_t devState = tcpip_convert_device_state(recv_buffer.state);
+                    memcpy(&((tcpipHostDeviceInfo_t*)(devices)+index)->info.state, &devState, sizeof(uint32_t));
+
+                    // check if IP matched with target IP
+                    if(strcmp(target_ip, ip_addr) == 0)
+                    {
+                        return TCPIP_HOST_DEVICE_FOUND;
+                    }
+
+                    index++;
+                }
+            }
+        } while((time(NULL) - start_time) <= DEVICE_DISCOVERY_RES_TIMEOUT_SEC);
+
+        // close socket
+        tcpip_close_socket(&socket_lookup[i]);
+    }
+
+    // return total device found
+    memcpy(device_count, &index, sizeof(index));
+
+    // free linked list
+    freeifaddrs(ifaddr);
+
+    return TCPIP_HOST_SUCCESS;
+}
---
+++ src/pc/protocols/tcpip_host.h
@@ -0,0 +1,102 @@
+/**
+ * @file    tcpip_host.h
+ * @brief   TCP/IP helper public header
+*/
+
+#ifndef TCPIP_HOST_H
+#define TCPIP_HOST_H
+
+/* **************************************************************************/
+/*      Include Files                                                       */
+/* **************************************************************************/
+#include "XLinkPlatform.h"
+#include "XLinkPublicDefines.h"
+
+/* **************************************************************************/
+/*      Public Macro Definitions                                            */
+/* **************************************************************************/
+#define TCPIP_LINK_SOCKET_PORT              11490
+#define BROADCAST_UDP_PORT                  11491
+
+#define MAX_IP_ADDR_CHAR                    64
+#define MAX_MXID_CHAR                       32
+#define MAX_IFACE_CHAR                      64
+#define MAX_DEVICE_DISCOVERY_IFACE          10
+
+#define MSEC_TO_USEC(x)                     (x * 1000)
+#define DEVICE_DISCOVERY_RES_TIMEOUT_SEC    1   
+#define DEVICE_RES_TIMEOUT_MSEC             100
+
+/* **************************************************************************/
+/*      Public Type Definitions                                             */
+/* **************************************************************************/
+/* TCP/IP error list */
+typedef enum
+{
+    TCPIP_HOST_DEVICE_FOUND = 1,
+    TCPIP_HOST_SUCCESS = 0,
+    TCPIP_HOST_DEVICE_NOT_FOUND = -1,
+    TCPIP_HOST_ERROR = -2,
+    TCPIP_HOST_TIMEOUT = -3,
+    TCPIP_HOST_DRIVER_NOT_LOADED = -4,
+    TCPIP_INVALID_PARAMETERS = -5
+} tcpipHostError_t;
+
+/* Host-to-device command list */
+typedef enum
+{
+    TCPIP_HOST_CMD_DEVICE_DISCOVER = 0,
+    TCPIP_HOST_CMD_DEVICE_INFO = 1,
+    TCPIP_HOST_CMD_RESET = 2
+} tcpipHostCommand_t;
+
+/* Device state */
+typedef enum
+{
+    TCPIP_HOST_STATE_BOOTED = 1,
+    TCPIP_HOST_STATE_BOOTLOADER = 3,
+    TCPIP_HOST_STATE_UNSUPPORTED
+} tcpipHostDeviceState_t;
+
+/* Device response payload */
+typedef struct
+{
+    char mxid[MAX_MXID_CHAR];
+    uint32_t state;
+} tcpipHostDeviceDiscoveryResp_t;
+
+/* Device information */
+typedef struct
+{
+    deviceDesc_t desc;
+    tcpipHostDeviceDiscoveryResp_t info;
+    int32_t linkSpeed;
+    int32_t linkFullDuplex;
+    int32_t gpioBootMode;
+} tcpipHostDeviceInfo_t;
+
+/* **************************************************************************/
+/*      Public Function Declarations                                        */
+/* **************************************************************************/
+
+/**
+ * @brief       Close socket
+ * 
+ * @param[in]   fd Pointer to socket
+ * @retval      TCPIP_HOST_ERROR Failed to close socket
+ * @retval      TCPIP_HOST_SUCCESS Success to close socket
+*/
+tcpipHostError_t tcpip_close_socket(void *fd);
+
+/**
+ * @brief       Broadcast message and get all devices responses with their IPs
+ * 
+ * @param[out]  devices Pointer to store device IP array
+ * @param[out]  device_count Total device IP address obtained
+ * @param[in]   target_ip Target IP address to be checked
+ * @retval      TCPIP_HOST_ERROR Failed to get network interface informations
+ * @retval      TCPIP_HOST_SUCCESS Received all device IP address available
+*/
+tcpipHostError_t tcpip_get_ip(tcpipHostDeviceInfo_t *devices, uint8_t* device_count, const char* target_ip);
+
+#endif /* TCPIP_HOST_H */
