cf59004e1e16d6a69c54eacce121d0d6b92213cf
TheMarpe <martin.peterlin7@gmail.com>
2021-11-28 02:45:44 +0100

    Fixed dependency - cmake libusb

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,8 +2,8 @@
 hunter_config(
     luxonis-libusb
     VERSION "1.0.24-cmake"
-    URL "https://github.com/luxonis/libusb/archive/2ea8c929dd28db443f8b7e8cdc37b12476f80eac.tar.gz"
-    SHA1 "e0f07065a9b232cd65d00434ee6eadd5d8c5028b"
+    URL "https://github.com/luxonis/libusb/archive/87129a4bc3ab25e40e68b4f01e83088af7cb60d4.tar.gz"
+    SHA1 "91739111f9c83cd5544a89ce0674e81cb58e0ded"
     CMAKE_ARGS
         WITH_UDEV=OFF
 )
