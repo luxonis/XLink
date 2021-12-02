db38ab19c80d074d11afbe106510b53a8ab5ac35
TheMarpe <martin.peterlin7@gmail.com>
2021-12-02 15:56:45 +0100

    Updated libusb

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,8 +2,8 @@
 hunter_config(
     libusb-luxonis
     VERSION "1.0.24-cmake"
-    URL "https://github.com/luxonis/libusb/archive/87129a4bc3ab25e40e68b4f01e83088af7cb60d4.tar.gz"
-    SHA1 "91739111f9c83cd5544a89ce0674e81cb58e0ded"
+    URL "https://github.com/luxonis/libusb/archive/57dc3a52a1ce09f617bd282c66c25485961e3361.tar.gz"
+    SHA1 "098ee4702ceee9ff65bb4e1f69280b3eccc38c95"
     CMAKE_ARGS
         WITH_UDEV=OFF
 )
