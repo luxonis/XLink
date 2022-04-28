967d752a0407a97f4e018d76f94c00f21e8d18bc
Martin Peterlin <martin.peterlin7@gmail.com>
2022-04-28 14:16:27 +0200

    Updated libusb and multiconfig generator support

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,8 +2,10 @@
 hunter_config(
     libusb-luxonis
     VERSION "1.0.24-cmake"
-    URL "https://github.com/luxonis/libusb/archive/b481f0d19499da513802259b96e433245306199b.tar.gz"
-    SHA1 "7b74e3850ef99fed7bb27107592a2906d1d99b9b"
+    URL "https://github.com/luxonis/libusb/archive/f52d62ae4216505558277aa6ea34e8fbca5804c7.tar.gz"
+    SHA1 "307ab83b0024df10800c60c246d9b2eca3b4f6b0"
     CMAKE_ARGS
         WITH_UDEV=OFF
+        # Build shared libs by default to not cause licensing issues
+        BUILD_SHARED_LIBS=ON
 )
