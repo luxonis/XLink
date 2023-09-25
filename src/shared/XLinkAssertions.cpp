3a5b814d709e1a009b5a634a8cd6bcb252e09f9b
asahtik <asahtik@gmail.com>
2023-09-25 14:16:47 +0200

    Fix assertions

--- src/shared/XLinkAssertions.cpp
+++ src/shared/XLinkAssertions.cpp
@@ -4,7 +4,7 @@
 
 static_assert(offsetof(xLinkEventHeader_t, id) == 0, "Offset to id is not 0");
 static_assert(offsetof(xLinkEventHeader_t, type) == 4, "Offset to type is not 4");
-static_assert(offsetof(xLinkEventHeader_t, streamName) == 8, "Offset to type is not 8");
-static_assert(offsetof(xLinkEventHeader_t, tnsec) == 60, "Offset to type is not 60");
-static_assert(offsetof(xLinkEventHeader_t, tsecLsb) == 64, "Offset to type is not 64");
-static_assert(offsetof(xLinkEventHeader_t, tsecMsb) == 68, "Offset to type is not 68");
+static_assert(offsetof(xLinkEventHeader_t, streamName) == 8, "Offset to streamName is not 8");
+static_assert(offsetof(xLinkEventHeader_t, tnsec) == 60, "Offset to tnsec is not 60");
+static_assert(offsetof(xLinkEventHeader_t, tsecLsb) == 64, "Offset to tsecLsb is not 64");
+static_assert(offsetof(xLinkEventHeader_t, tsecMsb) == 68, "Offset to tsecMsb is not 68");
