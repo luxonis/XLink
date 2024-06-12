8aa655bd8379b0a9a6577a7cd6e7fce2700735e8
Filippo Mutta <filippo.mutta@gmail.com>
2024-06-12 14:28:12 +0200

    Removed binary files.

--- examples/compile.sh
+++
@@ -1,2 +0,0 @@
-g++ xlink_server_local.cpp -g -L../build -lXLink -lusb-1.0 -I../include -o xlink_server_local
-g++ xlink_client_local.cpp -g -L../build -lXLink -lusb-1.0 -I../include -o xlink_client_local
