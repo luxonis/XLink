58edff585a9aca884325618af315f80a59d7462b
Filippo Mutta <filippo.mutta@luxonis.com>
2025-07-01 08:12:34 +0200

    Removed binary files

--- examples/compile.sh
+++
@@ -1,3 +0,0 @@
-g++ xlink_usb_server.cpp -g -I../include -L../build -lXLink -lc -lusb-1.0 -o xlink_usb_server
-g++ xlink_usb_client.cpp -g -I../include -L../build -lXLink -lc -lusb-1.0 -o xlink_usb_client
-
